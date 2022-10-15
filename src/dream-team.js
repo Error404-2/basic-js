c3a720ce726ac1a2d956d7f6cb40b8ed568ea5b7
error404-2 <kapwas@mail.ru>
2022-10-15 22:33:15 +0300

    feat: solved task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  //members.sort();
+  console.log(members);
+  if (!(members instanceof Array) /*!== 'object'*/) {
+    console.log("не массив");
+    return false;
+  }
+  if (members.length == 0) {
+    console.log("длина ноль");
+    return false;
+  }
+  let res = [];
+  for (let m of members) {
+    if (typeof m !== "string") {
+      continue;
+    }
+    if (m === " ") {
+      continue;
+    }
+    while (m[0] == " ") {
+      m = m.slice(1);
+    }
+    let fl = m.toUpperCase()[0];
+    res.push(fl);
+  }
+  res.sort();
+  let str = res.join("");
+  console.log(str);
+  return str;
   // remove line with error and write your code here
 }
 
 module.exports = {
   createDreamTeam
 };
+createDreamTeam(['Peter', 'Adam', 'Gary', 'Bobby', 'Philip', 'Andrea', 'Evelyn', 'Evelyn', 'Kimberly', 'Denise']);
+//, 'AABDEEGKPP');
+createDreamTeam(3); //false;
+createDreamTeam(3.312312);//, false);
+createDreamTeam(false);//, false);
+createDreamTeam(null);//, false);
+createDreamTeam(undefined);//, false);
+createDreamTeam({ 'foo': 'bar' });//, false)
