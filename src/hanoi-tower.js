467073620ba20a393d5abdfeb63e643534d73d0f
error404-2 <kapwas@mail.ru>
2022-10-16 18:44:32 +0300

    feat: solved task hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  console.log("дано", disksNumber, turnsSpeed);
+  let turns, seconds;
+  if (disksNumber === 1) {
+    turns = 1;
+  } else {
+    turns = 0;
+    for (i = 0; i < disksNumber; i++) {
+      turns += 2 ** i;
+      console.log("число операций", turns);
+    }
+  }
+  seconds = Math.floor(turns / turnsSpeed * 3600);
+  let obj = {
+    'turns': turns,
+    'seconds': seconds,
+  };
+  //obj['turns'] = turns;
+  //obj['seconds'] = seconds;
+  console.log(obj);
+  return obj;
   // remove line with error and write your code here
 }
 
 module.exports = {
   calculateHanoi
 };
+//calculateHanoi(3, 10); //{ turns: 31, seconds: 27 });
