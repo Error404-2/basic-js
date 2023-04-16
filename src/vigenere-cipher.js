4f2607f204dd60819aa8547afd989c8d20357313
error404-2 <kapwas@gmail.com>
2023-04-16 23:41:29 +0300

    feat: throw error viginere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,24 +19,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+let sdvig = "abcdefghijklmnopqrstuvwxyz";
+sdvig = sdvig.toUpperCase();
+
 class VigenereCipheringMachine {
   constructor(flag = true) {
     this.flag = flag;
-    console.log("========================", this.flag);
   }
 
-  encrypt() {
-
+  encrypt(message, key) {
+    if ((!message) || (!key)) {
+      throw new Error("Incorrect arguments!")
+    }
+    this.message = message;
+    this.key = key;
+    let strRes = [];
+    for (i = 0; i < this.message.length; i++) {
+      let position = sdvig.indexOf(this.message[i]);
+      let keyPosition = i % this.key.length;
+      let indexKey = sdvig.indexOf(this.key[keyPosition]);
+      let sumPosition = (position + indexKey) % 26;
+      strRes.push(sdvig[sumPosition]);
+    }
+    let res = strRes.join('');
+    console.log("+++++++++++++++++", strRes, res);
+    return res;
   }
-  decrypt() {
-
+  decrypt(encryptedMessage, key) {
+    if ((!encryptedMessage) || (!key)) {
+      throw new Error("Incorrect arguments!")
+    }
+    this.encryptedMessage = encryptedMessage;
+    this.key = key;
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
-
-
-
-
