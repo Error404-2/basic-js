436aa350cd8dbf8370e644cbc38a95d7251a5796
error404-2 <kapwas@gmail.com>
2023-04-17 23:38:33 +0300

    feat: simple chaining

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,36 +5,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  list: [],
   getLength(chain) {
     let res = chain.split("~~");
     console.log(res);
     return res.length;
-    // remove line with error and write your code here
   },
-  addLink(value) {
-    if (typeof value != 'number') {
-      throw new Error("You can\'t remove incorrect link!")
+  addLink(value = "") {
+    // if (typeof value != 'number') {
+    //   throw new Error("You can\'t remove incorrect link!")
+    // }
+    if (this.list.length > 0) {
+      this.list.push(`( ${value} )`);
+      // console.log("addlink=====", this.list);
+      return this;
+    } else {
+      this.list.push(`( ${value} )`);
+      // console.log("addlink=====", this.list);
+      return this;
     }
-    console.log("addLink");
-    // remove line with error and write your code here
   },
   removeLink(position) {
-    if (typeof position != 'number') {
-      throw new Error("You can\'t remove incorrect link!")
+    if (!((typeof position === "number") && (Number.isInteger(position)) && (position > 0) && (position <= this.list.length))) {
+      throw new Error("You can\'t remove incorrect link!");
     }
-    console.log("removeLink");
-    // remove line with error and write your code here
+    this.list.splice(position - 1, 1);
+    // console.log("this.list.splice(position - 1, 1)", this.list);
+    return this;
   },
   reverseChain() {
-    console.log("reverseChain");
-    // remove line with error and write your code here
+    this.list.reverse();
+    return this;
   },
   finishChain() {
-    console.log("finishChain");
-    // remove line with error and write your code here
+    return this.list.join("~~");
   }
 };
 
 module.exports = {
   chainMaker
 };
+
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0);
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink('2nd');
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2);
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(3);
+
+// chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain() //'( 3rd )~~( function () { } )';
